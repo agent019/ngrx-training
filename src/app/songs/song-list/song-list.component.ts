b1b48e287dce15d0cc9f6a3e7294e714692b2443
Reece Camper <reececamper@cox.net>
2020-04-22 12:27:31 -0700

    + missed change

--- src/app/songs/song-list/song-list.component.ts
+++ src/app/songs/song-list/song-list.component.ts
@@ -3,8 +3,34 @@ import { SongStateFacade } from '../+state/song-state.facade';
 
 @Component({
   selector: 'app-song-list',
-  templateUrl: './song-list.component.html',
-  styleUrls: ['./song-list.component.scss'],
+  template: `
+  <div class="container" style="padding: 20px;">
+    <div class="card">
+      <div class="card-header">
+        Songs:
+        <div style="float: right;">
+            <input placeholder="search" (input)="facade.onSearch($event.currentTarget.value)" [value]="facade.searchFilter$ | async" />
+        </div>
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Artist</td>
+            <td>Length</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr *ngFor="let song of facade.songs$ | async" [routerLink]="['/', 'songs', 'info', song.songId]">
+            <td>{{ song.title }}</td>
+            <td>{{ song.artist }}</td>
+            <td>{{ song.length }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+`
 })
 export class SongListComponent {
   constructor(public facade: SongStateFacade) {}
